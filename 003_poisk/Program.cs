32f5177852ec0e76e9af29b864f15f6ef78bd6f7
Tatiana <lulu.kep@yandex.ru>
2022-07-11 22:27:45 +0400

    003

---
+++ 003_poisk/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 4, 6, 556, 46, 5, 556};
+
+int n = array.Length;
+int find = 556;
+int index = 0;
+
+while (index<n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index ++;
+}
